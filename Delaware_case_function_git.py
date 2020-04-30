c9121b18761baf4e12ba3ac355dad555774c1507
MedicalFrank <64445321+MedicalFrank@users.noreply.github.com>
2020-04-30 15:37:54 -0400

    Add files via upload

---
+++ Delaware_case_function_git.py
@@ -0,0 +1,25 @@
+import tweepy
+import bs4
+import requests
+
+auth = tweepy.OAuthHandler('','') #API key is first, then API key secert is second
+auth.set_access_token('','') #Acess token, Access token secret is the order
+
+
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True) #wait_on_rate_limit will take a break when you are making too many api requests
+user = api.me()
+
+
+
+def get_DE_COVID_Case (NYTUrl):
+    res = requests.get(NYTUrl)
+    res.raise_for_status()
+    
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    elems = soup.select('#cases-top-presentation > div.g-asset.g-svelte.top-counts.g-asset-width-full > div > div > div:nth-child(1) > div.num.svelte-1iocbin')
+    return elems[0].text
+
+
+DEcasecount = get_DE_COVID_Case ('https://www.nytimes.com/interactive/2020/us/delaware-coronavirus-cases.html')
+print('The total COVID-19 cases, according to @nytimes, in Delaware, USA are ' + DEcasecount + '.')
+api.update_status('The total COVID-19 cases, according to @nytimes, in Delaware, USA are ' + DEcasecount + '.')
