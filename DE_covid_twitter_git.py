ff5933a304f3dfb7adac7a62e213fbce3e919f4b
MedicalFrank <64445321+MedicalFrank@users.noreply.github.com>
2020-04-30 15:36:24 -0400

    Update DE_covid_twitter_git.py

--- DE_covid_twitter_git.py
+++ DE_covid_twitter_git.py
@@ -1,7 +1,5 @@
 import tweepy #Twitter (pip install tweepy in command line)
 import time
-import bs4
-import requests
 
 auth = tweepy.OAuthHandler('','') #API key is first, then API key secert is second
 auth.set_access_token('','') #Acess token, Access token secret is the order
@@ -12,19 +10,6 @@ user = api.me()
 
 numberTweets = 500
 
-def get_DE_COVID_Case (NYTUrl):
-    res = requests.get(NYTUrl)
-    # must program in a get request that pulls info peroidically
-    res.raise_for_status()
-    
-    soup = bs4.BeautifulSoup(res.text, 'html.parser')
-    elems = soup.select('#cases-top-presentation > div.g-asset.g-svelte.top-counts.g-asset-width-full > div > div > div:nth-child(1) > div.num.svelte-ath6yh')
-    return elems[0].text
-
-#DEcasecount = get_DE_COVID_Case ('https://www.nytimes.com/interactive/2020/us/delaware-coronavirus-cases.html')
-
-#print('The total COVID-19 cases in Delaware, USA are ' + DEcasecount)
-#api.update_status('The total COVID-19 cases, according to @nytimes, in Delaware, USA are ' + DEcasecount + '.')
 
 for tweet in tweepy.Cursor(api.search, q="(Delaware AND COVID) OR (Delaware AND Coronavirus) OR (deCOVID) OR (coronavirusdelaware) OR (Delaware AND FlattenTheCurve) OR (socialdistancing AND Delaware) OR (netDE AND COVID) OR (netDE AND Coronavirus) OR (inWilm and COVID) OR (inWilm and Coronavirus) OR (#WilmDE and COVID) OR (#WilmDE and Coronavirus) -(Ohio OR 'Delaware Valley' OR 'Delaware County' OR 'Delaware River')", lang="en", ).items(numberTweets):
     try:
