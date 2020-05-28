bffaf9610a90865512759ed11daee6570c8c5fec
Franklin I <64445321+MedicalFrank@users.noreply.github.com>
2020-05-27 20:25:43 -0400

    Updated the url source and selector for the scrape
    For some reason, the NYTimes page was not responsive to my function so I changed to the State of Delaware Dashboard

--- Delaware_case_function_git.py
+++ Delaware_case_function_git.py
@@ -8,15 +8,17 @@ auth.set_access_token('','') #Acess token, Access token secret is the order
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True) #wait_on_rate_limit will take a break when you are making too many api requests
 user = api.me()
 
-def get_DE_COVID_Case (NYTUrl):
-    res = requests.get(NYTUrl)
+def get_DE_COVID_Case (DHSSUrl):
+    res = requests.get(DHSSUrl)
     res.raise_for_status()
     
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
-    elems = soup.select('#cases-top-presentation > div.g-asset.g-svelte.top-counts.g-asset-width-full > div > div > div:nth-child(1) > div.num.svelte-1iocbin')
+    elems = soup.select('#overview > div > article > div > div > div.col-md-12.col-lg-6.d-flex.flex-column > div.c-dashboard-card.c-dashboard-card--simple.c-dashboard-card--stretch.mb-4 > div.c-dashboard-card__content-wrapper > div > div > div.c-summary-metric__group-wrapper.col-lg-12.col-xl-4.mb-4 > div > div.c-summary-metric__label-value-group > span.c-summary-metric__value.c-summary-metric__value--lg.d-block')
     return elems[0].text
 
 
-DEcasecount = get_DE_COVID_Case ('https://www.nytimes.com/interactive/2020/us/delaware-coronavirus-cases.html')
-print('The total COVID-19 cases, according to @nytimes, in Delaware, USA are ' + DEcasecount + '.')
-api.update_status('The total COVID-19 cases, according to @nytimes, in Delaware, USA are ' + DEcasecount + '.')
+DEcasecount = get_DE_COVID_Case ('https://myhealthycommunity.dhss.delaware.gov/locations/state')
+print('The total COVID-19 cases, according to @Delaware_DHSS, in Delaware, USA are ' + DEcasecount + '.')
+api.update_status('The total COVID-19 cases, according to @Delaware_DHSS, in Delaware, USA are' + DEcasecount+ '.')
+
+
